9eee43d33fb14c180c5f53fa630fb38482a84c7d
MrHeer <hlm52pk@163.com>
2017-05-04 22:52:42 +0800

    add BiThrTree.h etc.

---
+++ Algorithm/InThreading.cpp
@@ -0,0 +1,27 @@
+#include "BiThrTree.h"
+
+// 线索化
+void InThreading(BiThrTree p)
+{
+	BiThrTree pre = p;
+	if (p)
+	{
+		InThreading(p->lchild);		// 左子树线索化
+
+		// 前驱线索
+		if (!p->lchild)
+		{
+			p->LTag = Thread;
+			p->lchild = pre;
+		}
+
+		// 后继线索
+		if (!pre->rchild)
+		{
+			pre->RTag = Thread;
+			pre->rchild = p;
+		}
+		pre = p;					// 保持pre指向p的前驱
+		InThreading(p->rchild);		// 右子树线索化
+	}
+}//	InThreading
